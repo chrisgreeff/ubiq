a7d5a680f5b11f8a85b46b9043ea55681fefbc48
chrisgreeff <chris.greeff@vendhq.com>
2015-09-20 15:21:20 +1200

    Progress, but autoplay still not working

--- assets/scripts/ubiq/landing.js
+++ assets/scripts/ubiq/landing.js
@@ -1,16 +1,13 @@
-(function () {
-  var player = $f($('.uq-landing-video-fullscreen-iframe')[0])
-
-  player.addEvent('ready', function() {
-    console.log(arguments)
-    player.api('play')
-  })
+$(function () {
+  var player = $f($('#uq-landing-video-fullscreen-iframe')[0]);
 
   $('.uq-landing-play').on('click', function() {
     $('.uq-landing-video-fullscreen-container').addClass('visible')
+    player.api('play')
   })
 
   $('.uq-landing-video-fullscreen-close').on('click', function() {
     $('.uq-landing-video-fullscreen-container').removeClass('visible')
+    player.api('unload')
   })
-}())
+})
