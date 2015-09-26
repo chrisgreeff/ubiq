bc3af578ed5bed4443edcc5a2c9de21c64390c0b
chrisgreeff <chris.greeff@vendhq.com>
2015-09-26 16:08:55 +1200

    Some commmits

--- gulp/tasks/standard.js
+++ gulp/tasks/standard.js
@@ -1,8 +1,16 @@
 import gulp from 'gulp'
 import standard from 'gulp-standard'
 
+const STANDARD_GLOB = [
+  './**/*.js',
+  '!./node_modules/**/*',
+  '!./assets/scripts/vendor/**/*',
+  '!./dist/**/*.js',
+  '!./assets/scripts/util/**/*.js'
+]
+
 gulp.task('standard', () => {
-  return gulp.src(['./**/*.js', '!./node_modules/**/*'])
+  return gulp.src(STANDARD_GLOB)
     .pipe(standard())
     .pipe(standard.reporter('default', {}))
 })
